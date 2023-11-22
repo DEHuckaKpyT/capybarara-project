dfbaabebbdfd91cb63a491cb1b831eb6712c07a7
DEHuckaKpyT <den-matytsin@mail.ru>
2023-11-22 13:13:30 +1000

    добавлен parallax, валидация инпутов в форме

---
+++ resources/js/sections/contact-section.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    makeContactButtonActive()
+})
+
+async function makeContactButtonActive() {
+    const contactForm = document.forms.contact;
+    const requiredFields = [contactForm.subject, contactForm.name, contactForm.phone]
+
+    const checkerAll = async function checkRequiredInputs() {
+        for (let i = 0; i < requiredFields.length; i++) {
+            if (isBlank(requiredFields[i].value)) {
+                contactForm.submitButton.disabled = true
+                return
+            }
+        }
+        contactForm.submitButton.disabled = false
+    }
+    const checkerCurrent = async function checkRequiredInputs(event) {
+        const field = event.target
+
+        if (isBlank(field.value)) {
+            field.classList.add('contact-section__form-required-input')
+            field.parentNode.classList.add('contact-section__form-required-label') // здесь это всегда label
+        } else {
+            field.classList.remove('contact-section__form-required-input')
+            field.parentNode.classList.remove('contact-section__form-required-label') // здесь это всегда label
+        }
+    }
+
+    for (let i = 0; i < requiredFields.length; i++) {
+        requiredFields[i].addEventListener('input', checkerAll)
+        requiredFields[i].addEventListener('blur', checkerCurrent)
+    }
+}
+
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
