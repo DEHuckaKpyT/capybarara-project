111f3218eaefe8a97447f42f16321e4bc9fd8c3f
DEHuckaKpyT <den-matytsin@mail.ru>
2023-11-24 14:37:39 +1000

    добавил точки с запятой в js

--- resources/js/sections/contact-section.js
+++ resources/js/sections/contact-section.js
@@ -1,35 +1,35 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    makeContactButtonActive()
-})
+    makeContactButtonActive();
+});
 
 async function makeContactButtonActive() {
     const contactForm = document.forms.contact;
-    const requiredFields = [contactForm.subject, contactForm.name, contactForm.phone]
+    const requiredFields = [contactForm.subject, contactForm.name, contactForm.phone];
 
     const checkerAll = async function checkRequiredInputs() {
         for (let i = 0; i < requiredFields.length; i++) {
             if (isBlank(requiredFields[i].value)) {
-                contactForm.submitButton.disabled = true
-                return
+                contactForm.submitButton.disabled = true;
+                return;
             }
         }
-        contactForm.submitButton.disabled = false
-    }
+        contactForm.submitButton.disabled = false;
+    };
     const checkerCurrent = async function checkRequiredInputs(event) {
-        const field = event.target
+        const field = event.target;
 
         if (isBlank(field.value)) {
-            field.classList.add('contact-section__form-error-input')
-            field.parentNode.classList.add('contact-section__form-error-label') // здесь это всегда label
+            field.classList.add('contact-section__form-error-input');
+            field.parentNode.classList.add('contact-section__form-error-label'); // здесь это всегда label
         } else {
-            field.classList.remove('contact-section__form-error-input')
-            field.parentNode.classList.remove('contact-section__form-error-label') // здесь это всегда label
+            field.classList.remove('contact-section__form-error-input');
+            field.parentNode.classList.remove('contact-section__form-error-label'); // здесь это всегда label
         }
     }
 
     for (let i = 0; i < requiredFields.length; i++) {
-        requiredFields[i].addEventListener('input', checkerAll)
-        requiredFields[i].addEventListener('blur', checkerCurrent)
+        requiredFields[i].addEventListener('input', checkerAll);
+        requiredFields[i].addEventListener('blur', checkerCurrent);
     }
 }
 
